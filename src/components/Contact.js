51b7e3332da412c90cc30b64d88f409ec0f6c0f8
Skylar Kesselring <skylarkesselring@gmail.com>
2022-06-16 18:37:47 -0400

    Add project section & basic styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ export default function Contact() {
   return(
     <section className="contact" id="contact">
       <h1>Contact</h1>
-      <h3>you gay(happy)</h3>
+      <h3>When a kid holds a balloon he prevents it from floating awaying, but</h3>
     </section>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,27 @@ export default function Projects() {
 
   return(
     <section className="projects" id="projects">
-      <h1>PROJECTS</h1>
+      <h1 className='projects--title'>PROJECTS</h1>
+      
+      <div className='projects--container'>
+        <div className='projects--project -trivia'>
+          <h4>Quizzical</h4>
+          <hr className='line' />
+          <p>A trivia game</p>
+        </div>
+        <div className='projects--project -portfolio'>
+          <h4>Personal Website</h4>
+          <hr className='line' />
+          <p>The website you're currently on (thanks for visiting!)</p>
+        </div>
+        <div className='test-div'>
+        <div className='projects--project -tenzies'>
+          <h4>Tenzies</h4>
+          <hr className='line' />
+          <p>Dice game</p>
+        </div>
+        </div>
+      </div>
     </section>
   );
 }
